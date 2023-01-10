ce2586531ccbad12ed404a081223ebb656381154
Marcos Vinicius <94012938+kddez@users.noreply.github.com>
2023-01-10 14:30:18 -0300

    aprendendo c/ eclipse

---
+++ aprendendo-com-eclipse/src/com/stefanini/aprendendocomeclipse/aula/Aula1.java
@@ -0,0 +1,8 @@
+package com.stefanini.aprendendocomeclipse.aula;
+
+public class Aula1 {
+	public static void main(String[] args) {
+        Aula2 aula = new Aula2(); 
+        System.out.println(aula);
+}
+}

14049def558669ab101f1d067ebf9a6255228ffc
Marcos Vinicius <94012938+kddez@users.noreply.github.com>
2023-01-10 15:06:41 -0300

    variáveis

---
+++ curso-java-basico/src/com/stefanini/cursojavabasico/variaveis/Variaveis.java
@@ -0,0 +1,33 @@
+package com.stefanini.cursojavabasico.variaveis;
+
+public class Variaveis {
+	
+	public static void main(String[] args) {
+		
+		
+		int anoAtual =  2023; 
+		byte cento = 100;  
+		short numShort = 12321;
+		long numLong = 2147483647;
+		
+		float numFloat = 10.000f;
+		double numDouble = 10000000000000.000;
+		
+		char varChar = 'F';
+		
+		boolean varBool = true;
+		
+		String diaDaSemana = "Sexta-feira"; 
+		Integer numeroConta = 4000;
+		Long numeroLongo = 2323232323L;
+		
+		System.out.println(numeroLongo);
+		
+		System.out.println(diaDaSemana);
+		
+		System.out.println(cento);
+		
+		System.out.println(numDouble);
+		
+	}
+}
